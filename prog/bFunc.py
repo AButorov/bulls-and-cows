92a009104e8d2307d1438c54de85caf67308c684
AButorov <40419739+AButorov@users.noreply.github.com>
2018-12-15 20:33:23 +0700

    work!

--- prog/bFunc.py
+++ prog/bFunc.py
@@ -3,10 +3,17 @@ import random
 class bBullsCows:
     ''' main class '''
     def __init__(self, iDigit):
+        # количество цифр
         self.iDigit = iDigit
+        # задуманный код
         self.sCode  = []
+        # введенный код пользователем
         self.sInput = []
-    
+        # количество быков
+        self.iBulls = 0
+        # количество коров
+        self.iCows  = 0
+
     def genCode(self):
         i = 0
         while i < self.iDigit:
@@ -16,13 +23,38 @@ class bBullsCows:
 
     def getUserCode(self):
         sTmp = input('input '+str(self.iDigit)+' digit:')
+        self.sInput.clear()
         i = 0
         while i < self.iDigit:
              self.sInput.append(int(sTmp[i:(i+1)])) 
              i += 1
             
+    def compareCodes(self):
+        bFlag = True
+        self.iBulls = 0
+        self.iCows  = 0
+        i=0
+        while i<self.iDigit:
+            if self.sCode[i] == self.sInput[i]:
+                self.iBulls += 1
+            else:
+                bFlag = False
+            j=0
+            while j<self.iDigit:
+                if self.sCode[i] == self.sInput[j]:
+                    self.iCows += 1
+                j += 1
+            i += 1
+        return bFlag
 
     def run(self):
         self.genCode()
         self.getUserCode()
-        print('code: ',self.sCode,'user:',self.sInput)
+        itry = 1
+        while self.compareCodes() == False:
+            print ('№',itry, 'Bulls:', self.iBulls, 'Cows', self.iCows)
+            print(' code: ',self.sCode,'user:',self.sInput)
+            itry += 1
+            self.getUserCode()
+        else:
+            print('winner!!!  ', itry)
